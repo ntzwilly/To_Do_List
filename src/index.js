90ff59d6987c10b2fbb1574b7045165010e943f2
ntzwilly <ntzwilly@gmail.com>
2021-07-22 16:05:32 +0200

    correct linter errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import "./style.css";
-import Icon from "./enter.svg";
-import recycle from "./recycle.svg";
-import { dragAndDrop } from "./interactive.js";
-import { checkboxChange, update } from "./status.js";
+/* eslint-disable import/no-cycle */
+import './style.css';
+import Icon from './enter.svg';
+import recycle from './recycle.svg';
+import { dragAndDrop } from './interactive.js';
 
 export function elementGenerator(typeName, className, content, idName) {
   const element = document.createElement(typeName);
@@ -17,54 +17,53 @@ export function elementGenerator(typeName, className, content, idName) {
   }
   return element;
 }
-
+/* eslint-disable import/no-mutable-exports */
 export let todoTasks = [
   {
-    description: "Read the last chapiter of Ruby book",
+    description: 'Read the last chapiter of Ruby book',
     completed: false,
     index: 0,
   },
   {
-    description: "Grab a cup of coffee",
+    description: 'Grab a cup of coffee',
     completed: true,
     index: 1,
   },
   {
-    description: "Go to the marked",
+    description: 'Go to the marked',
     completed: false,
     index: 2,
   },
 ];
 
-
-const todo = elementGenerator("div", "container", null, null);
-const todoHeader = elementGenerator("div", "title", null, null);
-const header = elementGenerator("div", "to-do-title", null, null);
+const todo = elementGenerator('div', 'container', null, null);
+const todoHeader = elementGenerator('div', 'title', null, null);
+const header = elementGenerator('div', 'to-do-title', null, null);
 header.textContent = "Today's To Do";
 todoHeader.appendChild(header);
 
 const myRecycle = new Image();
 myRecycle.src = recycle;
-myRecycle.classList.add("recycle");
+myRecycle.classList.add('recycle');
 todoHeader.appendChild(myRecycle);
 
-const form = elementGenerator("form", "to-do", null, null);
-const taskInput = elementGenerator("input", "add-to-do", null, null);
-taskInput.placeholder = "Add to your list...";
+const form = elementGenerator('form', 'to-do', null, null);
+const taskInput = elementGenerator('input', 'add-to-do', null, null);
+taskInput.placeholder = 'Add to your list...';
 form.appendChild(taskInput);
 
 const enterIcon = new Image();
 enterIcon.src = Icon;
-enterIcon.classList.add("enter-icon");
+enterIcon.classList.add('enter-icon');
 
 form.appendChild(enterIcon);
 
-export const todoList = elementGenerator("ul", "to-do-list", null, null);
+export const todoList = elementGenerator('ul', 'to-do-list', null, null);
 
-const divClear = elementGenerator("div", "div-clear", null, null);
-const btnClear = elementGenerator("button", "clear", null, null);
-btnClear.type = "button";
-btnClear.textContent = "Clear All completed";
+const divClear = elementGenerator('div', 'div-clear', null, null);
+const btnClear = elementGenerator('button', 'clear', null, null);
+btnClear.type = 'button';
+btnClear.textContent = 'Clear All completed';
 divClear.appendChild(btnClear);
 
 todo.appendChild(todoHeader);
@@ -72,12 +71,12 @@ todo.appendChild(form);
 todo.appendChild(todoList);
 todo.appendChild(divClear);
 
-const toDoContainer = document.getElementById("todo-container");
+const toDoContainer = document.getElementById('todo-container');
 toDoContainer.appendChild(todo);
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   const result = localStorage.getItem('ToDo');
-  if(result){
+  if (result) {
     todoTasks = JSON.parse(result);
   }
   dragAndDrop();
--- src/interactive.js
+++ src/interactive.js
@@ -1,84 +1,87 @@
-import moreIcon from "./more.svg";
-import { todoList, todoTasks, elementGenerator } from "./index.js";
+/* eslint-disable import/no-cycle */
+import moreIcon from './more.svg';
+import { todoList, todoTasks, elementGenerator } from './index.js';
 
-function listItem(elem){
-  const list = elementGenerator("li", "task draggable", null, null);
-    const flex = elementGenerator("div", "flex", null, null);
-    const oneTodo = elementGenerator("input", "one-todo", null, null);
-    oneTodo.type = "checkbox";
-    oneTodo.checked = elem.checked;
-    
-    const form = elementGenerator("form", "edit", null, null);
-    const input = elementGenerator("input", "label", null, null);
-    input.value = elem.description;
-    const image = elementGenerator("img", "more", null, null);
-    image.src = moreIcon;
-
-    if (elem.checked) {
-      input.classList.add("line-through");
-    } else {
-      input.classList.remove("line-through");
-    }
-
-    oneTodo.addEventListener("change", (e) => {
-     const todo = todoTasks[elem.index]
-     todo.checked = !elem.checked;
-     dragAndDrop();
-    });
-
-    form.appendChild(input);
-    flex.appendChild(oneTodo);
-    flex.appendChild(form);
-    list.appendChild(flex);
-    list.appendChild(image);
-
-    todoList.appendChild(list);
-
-    list.addEventListener("mousedown", () => {
-      list.setAttribute("draggable", true);
-    });
-
-    list.addEventListener("dragstart", (event) => {
-      event.dataTransfer.setData("index", elem.index);
-    });
-
-    list.addEventListener("drop", (event) => {
-      const draggedIndex = event.dataTransfer.getData("index");
-      const dropIndex = elem.index;
-
-      swap(draggedIndex, dropIndex);
-      list.setAttribute("draggable", false);
-    });
-
-    list.addEventListener("dragover", (event) => {
-      event.preventDefault();
-    });
-
-    return list;
+function savedList() {
+  localStorage.setItem('ToDo', JSON.stringify(todoTasks));
 }
 
+function listItem(elem) {
+  const list = elementGenerator('li', 'task draggable', null, null);
+  const flex = elementGenerator('div', 'flex', null, null);
+  const oneTodo = elementGenerator('input', 'one-todo', null, null);
+  oneTodo.type = 'checkbox';
+  oneTodo.checked = elem.checked;
+
+  const form = elementGenerator('form', 'edit', null, null);
+  const input = elementGenerator('input', 'label', null, null);
+  input.value = elem.description;
+  const image = elementGenerator('img', 'more', null, null);
+  image.src = moreIcon;
+
+  if (elem.checked) {
+    input.classList.add('line-through');
+  } else {
+    input.classList.remove('line-through');
+  }
+
+  oneTodo.addEventListener('change', () => {
+    const todo = todoTasks[elem.index];
+    todo.checked = !elem.checked;
+    /* eslint-disable no-use-before-define */
+    dragAndDrop();
+  });
+
+  form.appendChild(input);
+  flex.appendChild(oneTodo);
+  flex.appendChild(form);
+  list.appendChild(flex);
+  list.appendChild(image);
+
+  todoList.appendChild(list);
+
+  list.addEventListener('mousedown', () => {
+    list.setAttribute('draggable', true);
+  });
+
+  list.addEventListener('dragstart', (event) => {
+    event.dataTransfer.setData('index', elem.index);
+  });
+
+  function swap(draggedIndex, dropIndex) {
+    const dragged = todoTasks[draggedIndex];
+    const drop = todoTasks[dropIndex];
+
+    todoTasks[draggedIndex] = drop;
+    todoTasks[dropIndex] = dragged;
+
+    dragged.index = dropIndex;
+    drop.index = draggedIndex;
+
+    dragAndDrop();
+  }
+
+  list.addEventListener('drop', (event) => {
+    const draggedIndex = event.dataTransfer.getData('index');
+    const dropIndex = elem.index;
+
+    swap(draggedIndex, dropIndex);
+    list.setAttribute('draggable', false);
+  });
+
+  list.addEventListener('dragover', (event) => {
+    event.preventDefault();
+  });
+
+  return list;
+}
+
+/* eslint-disable import/prefer-default-export */
 export function dragAndDrop() {
-  todoList.innerHTML = "";
+  todoList.innerHTML = '';
   todoTasks.forEach((elem) => {
-   const item =  listItem(elem);
-   todoList.appendChild(item);
+    const item = listItem(elem);
+    todoList.appendChild(item);
   });
   savedList();
-}
-
-function swap(draggedIndex, dropIndex) {
-  const dragged = todoTasks[draggedIndex];
-  const drop = todoTasks[dropIndex];
-
-  todoTasks[draggedIndex] = drop;
-  todoTasks[dropIndex] = dragged;
-
-  dragged.index = dropIndex;
-  drop.index = draggedIndex;
-
-  dragAndDrop();
-}
-
-function savedList(){
-  localStorage.setItem('ToDo', JSON.stringify(todoTasks));
 }
